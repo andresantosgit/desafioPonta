00384c4bb85fb5f560a94df3e16309e1b3b017b6
ANDRE da Silva Santos C023580 <C023580@bnb.gov.br>
2024-04-29 13:55:53 -0300

    sso

--- src/BNB.SubscricaoCapitais.WebUI/Program.cs
+++ src/BNB.SubscricaoCapitais.WebUI/Program.cs
@@ -18,8 +18,11 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
-builder.Services.AddBNBAuth(options =>
+builder.Services.AddBNBAuthISKey(options =>
 {
+    options.KeycloakResourceFile = BNBAuthDefaults.KeycloakResourceFile;
+    options.ISKeyUrl = "http://s2iisg01.dreads.bnb:8076/BN.S267.Autorizador";
+    options.ISKeySiglaSistema = "493SUBCAP";
     options.ISKeyFilterEnabled = false;
 });
 
@@ -68,6 +71,6 @@ app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
 
-app.UseBNBAuth();
+app.UseBNBAuthISKey();
 
 app.Run();
